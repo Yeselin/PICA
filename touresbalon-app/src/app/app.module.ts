03b85cf95221fdd72bc260c66894cb220b47d862
Anders <andersbarrios@hotmail.com>
2018-09-27 16:57:31 -0500

    Not work the fucking search bar yet

--- touresbalon-app/src/app/app.module.ts
+++ touresbalon-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './component/search/search.component';
+import { MaincontentComponent } from './component/maincontent/maincontent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule
