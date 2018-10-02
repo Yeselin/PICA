a49fcb8edb0d7499ebd8c2ff00560261a9ac7ea0
anders3118 <andersbarrios@gmail.com>
2018-10-01 23:48:29 -0500

    Se crea service-registry

--- touresbalon-app/src/environments/environment.prod.ts
+++ touresbalon-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  API_PRODUCTS: 'http://localhost:8080/api/products',
-  API_ORDERS: 'http://localhost:8080/api/orders',
-  API_CUSTOMERS: 'http://localhost:8080/api/customers',
+  API_PRODUCTS: 'http://192.168.0.8/api/products',
+  API_ORDERS: 'http://192.168.0.8/api/orders',
+  API_CUSTOMERS: 'http://192.168.0.8/api/customers',
   STORAGE_KEY_SLIDER:'slider',
   SHOPPING_CART:'shopping_cart'
 };
