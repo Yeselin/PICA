2e797a1fba8b9cf8d497b5069a154df9fa67ea7c
salmon54 <38894518+salmon54@users.noreply.github.com>
2018-10-20 17:51:57 -0500

    Add files via upload

---
+++ 1 Script_Productos_SQL_Server_2012.sql
@@ -0,0 +1,111 @@
+
+CREATE TABLE city (
+    id          bigint ,
+    name_city   nvarchar (50) , 
+    cost NUMERIC (18,2) , 
+    country_name NVARCHAR (100) ,
+	country_short_name nvarchar(2),
+	create_date datetime,
+	update_date datetime,
+    CONSTRAINT city_pk PRIMARY KEY (id)
+)
+GO
+--268.832
+
+CREATE TABLE lodging (
+    id     bigint ,
+    name   nvarchar (50),
+	cost numeric(18,2),
+	create_date datetime,
+	update_date datetime,
+    CONSTRAINT lodging_pk PRIMARY KEY (id)
+)
+GO
+
+CREATE TABLE spectacle (
+    id     bigint ,
+    name   nvarchar(50),
+	cost numeric(18,2),
+	create_date datetime,
+	update_date datetime,
+	CONSTRAINT spectacle_pk PRIMARY KEY (id)
+)
+GO
+
+CREATE TABLE transport(
+    id     bigint ,
+    name   nvarchar(50),
+	cost numeric(18,2),
+	create_date datetime,
+	update_date datetime,
+	CONSTRAINT transport_pk PRIMARY KEY (id)
+)
+GO
+
+CREATE TABLE campana(
+    id     bigint ,
+    name   nvarchar(50),
+	percentage numeric(18,2),
+	create_date datetime,
+	update_date datetime,
+	start_date datetime,
+	effective_date datetime,
+    image_ref NVARCHAR (500),
+	product_id BIGINT,
+	is_active int,
+	CONSTRAINT campana_pk PRIMARY KEY (id)
+--	CONSTRAINT campana_product_fk FOREIGN KEY (product_id) REFERENCES product(id)
+)
+GO
+
+CREATE TABLE product (
+    id     bigint IDENTITY(1,1),
+    name   nvarchar (50) , 
+    spectacle_date DATE , 
+    arrival_date DATE , 
+    departure_date DATE , 
+    transport_type BIGINT , 
+    spectacle_type BIGINT , 
+    lodging_type BIGINT , 
+    description NVARCHAR (100) , 
+    code NVARCHAR (20) , 
+    image_ref NVARCHAR (500) , 
+    source_city BIGINT , 
+    target_city BIGINT ,
+    create_date datetime,
+    update_date datetime,
+	cost_total numeric(18,2),
+	status int
+	CONSTRAINT product_pk PRIMARY KEY (id),
+	CONSTRAINT product_source_city_fk FOREIGN KEY (source_city) REFERENCES city(id),
+	CONSTRAINT product_target_city_fk FOREIGN KEY (target_city) REFERENCES city(id),
+	CONSTRAINT product_transport_fk FOREIGN KEY (transport_type) REFERENCES transport(id),
+	CONSTRAINT product_lodging_fk FOREIGN KEY (lodging_type) REFERENCES lodging(id),
+	CONSTRAINT product_spectacle_fk FOREIGN KEY (spectacle_type) REFERENCES spectacle(id)
+)
+GO
+
+ALTER TABLE campana 
+	ADD CONSTRAINT campana_product_fk FOREIGN KEY (product_id) REFERENCES product(id)
+GO
+
+CREATE TABLE product2 (
+    id     bigint,
+    name   nvarchar (50) , 
+    spectacle_date DATE , 
+    arrival_date DATE , 
+    departure_date DATE , 
+    transport_type BIGINT , 
+    spectacle_type BIGINT , 
+    lodging_type BIGINT , 
+    description NVARCHAR (100) , 
+    code NVARCHAR (20) , 
+    image_ref NVARCHAR (500) , 
+    source_city BIGINT , 
+    target_city BIGINT ,
+    create_date datetime,
+    update_date datetime,
+	cost_total numeric(18,2),
+	status int
+)
+GO
---
+++ 5 INSERT_PRODUCT - TABLA INDEXADA.sql
@@ -0,0 +1,23 @@
+--Copia de la tabla no indexada PRODUCT2 a la tabla indexada PRODUCT
+INSERT INTO product
+SELECT
+name  ,
+spectacle_date,
+arrival_date,
+departure_date ,
+transport_type,
+spectacle_type ,
+lodging_type,
+description,
+code,
+image_ref,
+source_city ,
+target_city ,
+create_date ,
+update_date,
+cost_total,
+status
+FROM product2
+GO
+DROP TABLE PRODUCT2
+GO
