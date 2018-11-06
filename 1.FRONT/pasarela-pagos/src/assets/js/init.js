09d18692838f9a97b25aa5ea1e62aca55ba20e62
anders3118 <andersbarrios@gmail.com>
2018-11-05 19:01:11 -0500

    Se crea cascaron para pasarela de pagos

---
+++ 1.FRONT/pasarela-pagos/src/assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// SLIDER with jQuery
+$(document).ready(function(){
+  $('.slider').slider();
+});
